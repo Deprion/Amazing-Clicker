250fe5bd1f722390c91e3663c4606729eb735f91
Depri0n <Depri0n@yandex.ru>
2021-06-30 23:59:01 +0300

    so update

---
+++ Assets/Scripts/SO/LevelContainer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace SO
+{
+    [CreateAssetMenu(fileName = "StringExampleArray")]
+    public class LevelContainer : ScriptableObject
+    {
+        public int Level;
+        public StringRepresent[] StringRepresentArray;
+    }
+}
